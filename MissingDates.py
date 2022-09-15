640c27ea10e4bae720f44baa57a84fff00866116
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-09-14 19:42:26 -0500

    Missing prices working correctly

--- MissingDates.py
+++ MissingDates.py
@@ -5,29 +5,14 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/20220906Data2011_2020.csv')
 
-data2 = data[['fdate', 'ETHANOLrp', 'GASOLINErp']]
-print(data2.info())
+data2 = data.loc[:,['fdate', 'ETHANOLrp', 'GASOLINErp']]
+dateList = data2['fdate'].drop_duplicates().dropna().tolist()
+result = pd.DataFrame(columns=['fdate', 'ETHANOLrp_NaN', 'GASOLINErp_NaN'])
 
-#total NaNs
-print(data2['ETHANOLrp'].isna().sum())
-print(data2['GASOLINErp'].isna().sum())
-
-plt.plot(data)
-#per date
-"""for i in range(len(data2['ETHANOLrp'])):
-    #element = data2['ETHANOLrp'[i]].values
-    counter = 0
-    element = data2.iloc[i, 1]
-    check = np.isnan(element)
-
-result = pd.DataFrame(columns=['Date','NumberOfNaN'])
-    
-for i in range(len(data2['ETHANOLrp'])):
-    element = data2.iloc[i, 1]
-    date = data2.iloc[i,0]
-    check = np.isnan(element)
-    if check == True:
-        result.append({'Date':date, 'NumberOfNaN':1}, ignore_index = True)
-
+for i in range(len(dateList)):
+    date_df = data2[data2['fdate']==dateList[i]]
+    count = date_df['ETHANOLrp'].isna().sum()
+    row = pd.Series({'fdate': dateList[i], 'ETHANOLrp_NaN': date_df['ETHANOLrp'].isna().sum(), 'GASOLINErp_NaN':date_df['GASOLINErp'].isna().sum()})
+    result = pd.concat([result, row.to_frame().T], ignore_index=True)
 print(result)
-"""
+result.to_csv('/Users/marlenebultemann/Desktop/HTW/UM/result_missingByDate.csv')
--- MissingPricesByLocation.py
+++ MissingPricesByLocation.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/20220906Data2011_2020.csv')
 
-data2 = data.loc[:,['munic','fdate', 'ETHANOLrp', 'GASOLINErp']]
+data2 = data.loc[:2000,['munic','fdate', 'ETHANOLrp', 'GASOLINErp']]
 print(data2['ETHANOLrp'].isna().sum())
 print(data2['GASOLINErp'].isna().sum())
 municList = data2['munic'].drop_duplicates().dropna().tolist()
@@ -11,6 +11,7 @@ result = pd.DataFrame(columns=['munic', 'ETHANOLrp_NaN', 'GASOLINErp_NaN'])
 
 for i in range(len(municList)):
     munic_df = data2[data2['munic']== municList[i]].isna().sum()
+    print(munic_df)
     row = pd.Series({'munic': municList[i], 'ETHANOLrp_NaN': munic_df.loc['ETHANOLrp'], 'GASOLINErp_NaN':munic_df.loc['GASOLINErp']})
     result = pd.concat([result, row.to_frame().T], ignore_index=True)
 print(result)
