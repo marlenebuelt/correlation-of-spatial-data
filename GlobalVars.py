1264e488943a1922e9f0e3c6b81b3ceebd346c2b
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-09-29 15:49:33 -0500

    Missing Dates per Location - Loop file workinggit add GlobalVars.pygit add GlobalVars.py

--- GlobalVars.py
+++ GlobalVars.py
@@ -1,3 +1,5 @@
+import keyword
+from optparse import Values
 from tracemalloc import start
 import pandas as pd
 
@@ -51,7 +53,26 @@ def subperiod5_end():
     enddate = pd.to_datetime('2019-12-31')
     return enddate
 
-#iterate over this list?
-functions = [subperiod1_start(), subperiod1_end(), subperiod2_start(), subperiod2_end(), subperiod3_start(), subperiod3_end(),
-            subperiod4_start(), subperiod4_end(),subperiod5_start(), subperiod5_end()]
-print(functions)
+#iterate over this dict? --> might delete it, cant access it
+def SubperiodsDict():
+    global SubperiodsDict
+    SubperiodsDict = {'Subperiod 1': [subperiod1_start(), subperiod1_end()], 'Subperiod 2': [subperiod2_start(), subperiod2_end()], 
+        'Subperiod 3': [subperiod3_start(), subperiod3_end()], 'Subperiod 4': [subperiod4_start(), 
+        subperiod4_end()],'Subperiod 5': [subperiod5_start(), subperiod5_end()]}
+    return SubperiodsDict
+print(SubperiodsDict())
+print(SubperiodsDict.values())
+
+#lists
+def SubperiodsList():
+    global SubperiodsList
+    SubperiodsList = [subperiod1_start(), subperiod1_end(), subperiod2_start(), subperiod2_end(), 
+        subperiod3_start(), subperiod3_end(), subperiod4_start(), subperiod4_end(), subperiod5_start(), subperiod5_end()]
+    return SubperiodsList
+print(SubperiodsList())
+
+def SubperiodsNames():
+    global SubperiodsNames
+    SubperiodsNames = ['Subperiod1', 'Subperiod2', 'Subperiod3', 'Subperiod4', 'Subperiod5']
+    return SubperiodsNames
+print(SubperiodsNames())
--- MissingDatesPerLocation.py
+++ MissingDatesPerLocation.py
@@ -1,28 +1,34 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import GlobalVars
+import GlobalVars as gv
 
-#convert dates and drop anything after january 1st, 2020
-#this file will be used for creating new files for the different scenarios
+#TO DO: analysis stuff
 data = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/20220906Data2011_2020.csv')
 
 data2 = data.loc[:10000,['munic','fdate', 'ETHANOLrp']]
 data2['fdate'] = pd.to_datetime(data2['fdate'])
+
 totalobs = (data2['ETHANOLrp'].count())
 
 municList = data2['munic'].drop_duplicates().dropna().tolist()
-result = pd.DataFrame(columns=['munic', 'ETHANOLrp'])
+final = pd.DataFrame(columns=['munic'])
+subperiods_dict =gv.SubperiodsDict
+dates_helper = ['startdate', 'enddate']
+data3 = pd.DataFrame()
+data3 = data2
 
-#Subperiod 1
-#missing totals for Ethanol and Gasoline
-#munic_df = data2.loc[[data2['fdate']>GlobalVars.subperiod1_start()]:[data2['fdate']<GlobalVars.subperiod1_end()], :]
-data2 = data2.loc[data2['fdate']>GlobalVars.subperiod1_start()]
-data2 = data2.loc[data2['fdate']<GlobalVars.subperiod1_end()]
-print(data2)
-for i in range(len(municList)):
-    munic_df = data2[data2['munic']== municList[i]].isna().sum()
-    row = pd.Series({'munic': municList[i], 'ETHANOLrp': munic_df.loc['ETHANOLrp']})
-    result = pd.concat([result, row.to_frame().T], ignore_index=True)
-#result.to_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/result.csv')
-print(result)
+for key, value in subperiods_dict.items():
+    result = pd.DataFrame(columns=['munic'])
+    data3 = data2
+    for i in range(len(dates_helper)):
+        if dates_helper[i] == 'startdate':
+            startdate = value[i]
+        if dates_helper[i] == 'enddate':
+            enddate = value[i]
+            data3 = data3.loc[(data3['fdate']>startdate)]
+            data3 = data3.loc[(data3['fdate']<enddate)]
+            for i in range(len(municList)):
+                munic_df = data3[data3['munic']== municList[i]].isna().sum()
+                row = pd.Series({'munic': municList[i], key: munic_df.loc['ETHANOLrp']})
+                result = pd.concat([result, row.to_frame().T], ignore_index=True)
+    final = pd.merge(final, result, on='munic', how='outer')
+final.to_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/result.csv')
