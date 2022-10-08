d3cdd431c36641b38253d621223d30edc3dfbf67
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-10-08 19:07:25 -0400

    MissingDatesPerLocation returns correct values

--- MissingDatesPerLocation.py
+++ MissingDatesPerLocation.py
@@ -2,116 +2,96 @@ import pandas as pd
 import GlobalVars as gv
 
 #TO DO: Check Values
+#looks at seperate csv-files of dropped periods and returns values
 
-data = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/20220906Data2011_2020.csv')
-data2 = data.loc[:10000,['munic','fdate', 'ETHANOLrp']]
-data2['fdate'] = pd.to_datetime(data2['fdate'])
+#data = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/20220906Data2011_2020.csv')
+#data2 = data.loc[:10000,['munic','fdate', 'ETHANOLrp']]
+#data2['fdate'] = pd.to_datetime(data2['fdate'])
 
-totalobs = (data2['ETHANOLrp'].count())
+data_subp1 = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/data_subperiods/subperiod1.csv')
+data_subp2 = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/data_subperiods/subperiod2.csv')
+data_subp3 = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/data_subperiods/subperiod3.csv')
+data_subp4 = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/data_subperiods/subperiod4.csv')
+data_subp5 = pd.read_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/data_subperiods/subperiod5.csv')
 
-municList = data2['munic'].drop_duplicates().dropna().tolist()
+all_subp = [data_subp1, data_subp2, data_subp3, data_subp4, data_subp5]
+subp_list = gv.SubperiodsNames
+print(subp_list)
+
+for df in all_subp:
+    df = df.loc[:5000]
+    df['fdate'] = pd.to_datetime(df['fdate'])
+    totalobs = (df['ETHANOLrp'].count())
+
 final = pd.DataFrame(columns=['munic'])
-subperiods_dict =gv.SubperiodsDict
-dates_helper = ['startdate', 'enddate']
-data3 = pd.DataFrame()
-
-for key, value in subperiods_dict.items():
+"""
+#returns nothing new
+for df in all_subp:
+    municList = df['munic'].drop_duplicates().dropna().tolist()
     result = pd.DataFrame(columns=['munic'])
-    data3 = data2
-    for i in range(len(dates_helper)):
-        if dates_helper[i] == 'startdate':
-            startdate = value[i]
-        if dates_helper[i] == 'enddate':
-            enddate = value[i]
-            data3 = data3.loc[(data3['fdate']>startdate)]
-            data3 = data3.loc[(data3['fdate']<enddate)]
-            for i in range(len(municList)):
-                munic_df = data3[data3['munic']== municList[i]].isna().sum()
-                row = pd.Series({'munic': municList[i], key: munic_df.loc['ETHANOLrp']})
-                result = pd.concat([result, row.to_frame().T], ignore_index=True)
+    for i in range(len(municList)):
+        munic_df = df[df['munic']== municList[i]].isna().sum()
+        row = pd.Series({'munic': municList[i], 'key': munic_df.loc['ETHANOLrp']})
+        result = pd.concat([result, row.to_frame().T], ignore_index=True)
     final = pd.merge(final, result, on='munic', how='outer')
 #final.to_csv('/Users/marlenebultemann/Desktop/HTW/UM/correlation-of-spatial-data/result.csv')
 print(final)
-
+"""
 #Missing # of obs, missing % of obs, max length missing ---> DOUBLE CHECK VALUES
-dateList = data2['fdate'].drop_duplicates().dropna().tolist()
 list = ['Missing # of obs', 'Missing % of obs', 'max length']
 
 #missing # of obs
 final2= pd.DataFrame(columns=['Name'])
-for key, value in subperiods_dict.items():
-    i = 0
-    data3 = data2
+for i in range(len(all_subp)):
+    df = all_subp[i]
+    print(df)
     result = pd.DataFrame(columns=['Name'])
-    for i in range(len(dates_helper)):
-        if dates_helper[i] == 'startdate':
-            startdate = value[i]
-        if dates_helper[i] == 'enddate':
-            enddate = value[i]
-            data3 = data3.loc[(data3['fdate']>startdate)]
-            data3 = data3.loc[(data3['fdate']<enddate)]
-            row = pd.Series({'Name': list[0], key: data3['ETHANOLrp'].isna().sum()})
-            result = pd.concat([result, row.to_frame().T], ignore_index=True)
-            i = i +1
+    row = pd.Series({'Name': list[0], subp_list[i]: df['ETHANOLrp'].isna().sum()})
+    print(subp_list[i])
+    result = pd.concat([result, row.to_frame().T], ignore_index=True)
+    print(result) 
     final2 = pd.merge(final2, result, on=['Name'], how='outer')
 print(final2)
 
 #missing % of obs
 final3= pd.DataFrame(columns=['Name'])
-for key, value in subperiods_dict.items():
-    i = 0
+for i in range(len(all_subp)):
+    df = all_subp[i]
+    print(df)
     result2 = pd.DataFrame(columns=['Name'])
-    data3 = data2
-    print('start')
-    for i in range(len(dates_helper)):
-        if dates_helper[i] == 'startdate':
-            startdate = value[i]
-        if dates_helper[i] == 'enddate':
-            enddate = value[i]
-            data3 = data3.loc[(data3['fdate']>startdate)]
-            data3 = data3.loc[(data3['fdate']<enddate)]
-            #Missing # of obs
-            missing_number = data3['ETHANOLrp'].isna().sum()
-            i = i +1
-            #Missing % of obs
-            totalobs = (data3['ETHANOLrp'].count())
-            missing_percent = (missing_number/totalobs)
-            row = pd.Series({'Name': list[1], key: missing_percent})
-            result2 = pd.concat([result2, row.to_frame().T], ignore_index=True)
-            print(result2)
+    #Missing # of obs
+    missing_number = df['ETHANOLrp'].isna().sum()
+    print(missing_number)
+    #Missing % of obs
+    totalobs = (df['munic'].count()) #looking at the munic column to avoid issues with nan-values
+    print(totalobs)
+    missing_percent = (missing_number/totalobs)
+    print(missing_percent)
+    row = pd.Series({'Name': list[1], subp_list[i]: missing_percent})
+    result2 = pd.concat([result2, row.to_frame().T], ignore_index=True)
+    print(result2)
     final3 = pd.merge(final3, result2, on='Name', how='outer')
 print(final3)
 
 #maxlength
-data4 = data.loc[:10000,['munic','fdate', 'ETHANOLrp']]
 df = pd.DataFrame()
 result3= pd.DataFrame()
-
-final3= pd.DataFrame(columns=['Name'])
-for key, value in subperiods_dict.items():
-    i = 0
+maxmissingweeks = 4
+final4= pd.DataFrame(columns=['Name'])
+for i in range(len(all_subp)):
+    df = all_subp[i]
+    print(df)
     result4 = pd.DataFrame(columns=['Name'])
-    data3 = data2
-    print('start')
-    for i in range(len(dates_helper)):
-        if dates_helper[i] == 'startdate':
-            startdate = value[i]
-        if dates_helper[i] == 'enddate':
-            enddate = value[i]
-            data3 = data3.loc[(data3['fdate']>startdate)]
-            data3 = data3.loc[(data3['fdate']<enddate)]
-            
-            #Missing # of obs
-            missing_number = data3['ETHANOLrp'].isna().sum()
-            data3['Group']=data3.ETHANOLrp.notnull().astype(int).cumsum()
-            data3=data3[data3.ETHANOLrp.isnull()]
-            data3=data3[data3.Group.isin(data3.Group.value_counts()[data3.Group.value_counts()>4].index)]
-            data3['count']=data3.groupby('Group')['Group'].transform('size')
-            data3 = data3.drop_duplicates(['Group'], keep='first')
-            max = data3['count'].max()
-            result4 = result4.append(df, ignore_index=True)
-            row = pd.Series({'Name': list[1], key: max})
-            result4 = pd.concat([result4, row.to_frame().T], ignore_index=True)
-            i = i +1
-    final3 = pd.merge(final3, result4, on='Name', how='outer')
-print(final3)
+    #Missing cumulatives of obs
+    missing_number = df['ETHANOLrp'].isna().sum()
+    df['Group']=df.ETHANOLrp.notnull().astype(int).cumsum()
+    df=df[df.ETHANOLrp.isnull()]
+    df=df[df.Group.isin(df.Group.value_counts()[df.Group.value_counts()>maxmissingweeks].index)]
+    df['count']=df.groupby('Group')['Group'].transform('size')
+    df = df.drop_duplicates(['Group'], keep='first')
+    max = df['count'].max()
+    #result4 = result4.append(df, ignore_index=True)
+    row = pd.Series({'Name': list[2], subp_list[i]: max})
+    result4 = pd.concat([result4, row.to_frame().T], ignore_index=True)  
+    final4 = pd.merge(final4, result4, on='Name', how='outer')
+print(final4)
