9b1dfe369ce7a082c85194444bbfea76d754ab98
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-10-13 23:35:05 +0200

    now iterates over subperiod files

--- MissingDates.py
+++ MissingDates.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import SubperiodsDates as spd
 import SubperiodsPaths as spp
-# looks at the dates in detail
-df = spp.getOriginalFile()
+# #looks at dataframes of dropped periods in detail
+# NOT CHECKED YET
 
-dateList = df['fdate'].drop_duplicates().dropna().tolist()
-result = pd.DataFrame(columns=['fdate'])
 subp_dict = spd.SubperiodsDict
 subpall = spp.missingValsAll()
 counter = 0
 
-for key in subp_dict: 
+all_subp = spp.getAllSubP_drop1()
+subp_list = spd.SubperiodsNames
+
+for i in range(len(all_subp)):
+    subp = subp_list[i]
+    current_subp = all_subp[i]
     result_period = pd.DataFrame()
-    startenddatelist = subp_dict[key]
-    startdate = startenddatelist[0]
-    enddate = startenddatelist[1]
-    for i in range(len(dateList)):
-        date_df = df[(df['fdate']==dateList[i]) & (df['fdate']>startdate) & (df['fdate']<enddate)]
-        row = pd.Series({'fdate': dateList[i], key: date_df['ETHANOLrp'].isna().sum()})
+    dateList = current_subp['fdate'].drop_duplicates().dropna().tolist()
+    for j in range(len(dateList)):
+        date_df = current_subp[(current_subp['fdate']==dateList[j])]
+        row = pd.Series({'fdate': dateList[j], subp: date_df['ETHANOLrp'].isna().sum()})
         result_period = pd.concat([result_period, row.to_frame().T], ignore_index=True)
-    print(result_period)
     result_period.to_csv(subpall[counter])
     counter = counter + 1
