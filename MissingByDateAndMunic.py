a8bfd676e5dd076718770d62694a638d6bf74f79
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-10-21 16:18:44 +0200

    up and running

---
+++ MissingByDateAndMunic.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import SubperiodsDates as spd
+import SubperiodsPaths as spp
+
+#restructures data to easily see missing values
+# not double checked yet
+all_subp = spp.getAllSubP_afterdrop2()
+
+final = pd.DataFrame(columns=['munic'])
+final2= pd.DataFrame(columns=['Name'])
+resultcsvlist = spp.setMissingByDateAndMunicAll()
+
+for i in range(len(all_subp)):
+    df = all_subp[i]
+    result = pd.DataFrame()
+    final = df.loc[:,['fdate']]
+    municList = df['munic'].drop_duplicates().dropna().tolist()
+    for j in range(len(municList)):
+        df_munic = df[df['munic']==municList[j]]
+        result = df_munic.loc[:,['fdate']]
+        result[municList[j]] = df_munic.loc[:,['ETHANOLrp']]
+        final= pd.merge(final, result, on='fdate', how='left')
+    final.to_csv(resultcsvlist[i])
