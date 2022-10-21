3033adba2961aaa4b2e77c2da343459ddec2574d
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-10-21 20:46:28 +0200

    spatial correlation up and running

--- SpatialCorrelation.py
+++ SpatialCorrelation.py
@@ -2,50 +2,53 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 import SubperiodsPaths as spp
+import SubperiodsDates as spd
 
 #inspired by: https://builtin.com/data-science/exploratory-spatial-data-analysis-esda, 
-# combining dataframes: https://towardsdatascience.com/exploratory-spatial-data-analysis-esda-spatial-autocorrelation-7592edcbef9a
 #https://towardsdatascience.com/plotting-maps-with-geopandas-428c97295a73
 
-#overall file: will be replaced with files for scenarios
-all_df = spp.getAllSubP_drop1()
+all_df = spp.getAllSubP_afterdrop2()
+subperiodnames = spd.SubperiodsNames
 
-for df in all_df:
-    map_brazil = gpd.read_file(spp.getMapBrazilPath())
-    map_brazil.to_crs(epsg=4326).plot()
+map_brazil = gpd.read_file(spp.getMapBrazilPath())
+map_brazil.to_crs(epsg=4326)
 
-    crs = {'init':'EPSG:4326'}
+crs = {'init':'EPSG:4326'}
+
+"""for i in range(len(all_df)):
+    df = all_df[i]
+    
     geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
     geo_df = gpd.GeoDataFrame(df, crs = crs, geometry = geometry)
-
     fig, ax = plt.subplots(figsize = (10,10))
     map_brazil.to_crs(epsg=4326).plot(ax=ax, color='lightgrey')
-    map_brazil.plot(ax=ax, color='purple')
+    map_brazil.plot(ax=ax)
     geo_df.plot(ax=ax)
-    ax.set_title('Municipalities')
-
+    
     fig, ax = plt.subplots(figsize = (10,10))
     map_brazil.to_crs(epsg=4326).plot(ax=ax, color='lightgrey')
     geo_df.plot(ax=ax, alpha = .1 )
+    
+    ax.set_title('Municipalities' + subperiodnames[i])
+    plt.savefig('Municipalities' + subperiodnames[i])
+"""
 
-    ax.set_title('Municipalities')
-    plt.savefig('Municipalities')
-    plt.show()
+#missing municipalities summer 2015 --> looks at the original dataframe on aug 29th, 2015
+df1 = spp.getOriginalFile()
+df1 = df1[df1['fdate']=='2015-08-29']
+df1 = df1[df1['ETHANOLrp'].isna()]
 
-"""#step 2: munics we want to drop (list not final yet!)
-geometry = [Point(xy) for xy in zip(df_drop['longitude'], df_drop['latitude'])]
-geo_df2 = gpd.GeoDataFrame(df_drop, crs = crs, geometry = geometry)
 
+geometry = [Point(xy) for xy in zip(df1['longitude'], df1['latitude'])]
+geo_df = gpd.GeoDataFrame(df1, crs = crs, geometry = geometry)
 fig, ax = plt.subplots(figsize = (10,10))
-map_brazil.to_crs(epsg=4326).plot(ax=ax, color='red')
-map_brazil.plot(ax=ax, color='black')
-geo_df2.plot(ax=ax)
-ax.set_title('Municipalities_drop')
-
+map_brazil.to_crs(epsg=4326).plot(ax=ax, color='lightgrey')
+map_brazil.plot(ax=ax)
+geo_df.plot(ax=ax)
+    
 fig, ax = plt.subplots(figsize = (10,10))
-map_brazil.to_crs(epsg=4326).plot(ax=ax, color='pink')
-geo_df2.plot(ax=ax, alpha = .1 )
-
-ax.set_title('Municipalities_Drop')
-plt.savefig('Municipalities_drop')
-plt.show()"""
+map_brazil.to_crs(epsg=4326).plot(ax=ax, color='lightgrey')
+geo_df.plot(ax=ax, alpha = .1 )
+    
+ax.set_title('Municipalities missing in summer 2015')
+plt.savefig('Municipalities missing in summer 2015')
